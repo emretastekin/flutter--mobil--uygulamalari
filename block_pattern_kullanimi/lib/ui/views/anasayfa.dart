b5620cf16f6abc286d57a456809c8c545c2b90e6
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-03-14 23:04:44 +0300

    block pattern mimarisine giriş yapıldı ve hesap makinesi uygulamasında sonuca varıldı.

--- block_pattern_kullanimi/lib/ui/views/anasayfa.dart
+++ block_pattern_kullanimi/lib/ui/views/anasayfa.dart
@@ -1,4 +1,6 @@
+import 'package:block_pattern_kullanimi/ui/cubit/anasayfa_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Anasayfa extends StatefulWidget {
   const Anasayfa({super.key});
@@ -11,7 +13,6 @@ class _AnasayfaState extends State<Anasayfa> {
   var tfSayi1 = TextEditingController();
   var tfSayi2 = TextEditingController();
 
-  int sonuc = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,16 +22,21 @@ class _AnasayfaState extends State<Anasayfa> {
           padding: const EdgeInsets.only(left: 50.0, right: 50),
           child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text(sonuc.toString(), style: const TextStyle(fontSize: 50),),
+              BlocBuilder<AnasayfaCubit,int>( //Dinleme yapısı
+                builder: (context,sonuc){
+                  return Text(sonuc.toString(), style: const TextStyle(fontSize: 50),);
+
+                },
+              ),
               TextField(controller: tfSayi1, decoration: const InputDecoration(hintText: "Sayı 1"),),
               TextField(controller: tfSayi2, decoration: const InputDecoration(hintText: "Sayı 2"),),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ElevatedButton(onPressed: (){
-
+                    context.read<AnasayfaCubit>().toplamaYap(tfSayi1.text, tfSayi2.text);
                   }, child: const Text("Toplama", style: TextStyle(color: Colors.white),), style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue)),),
                   ElevatedButton(onPressed: (){
-
+                    context.read<AnasayfaCubit>().carpmaYap(tfSayi1.text, tfSayi2.text);
                   }, child: const Text("Çarpma", style: TextStyle(color: Colors.white),), style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue)),),
                 ],
               )
